b7f5e02a35fd354e3ef324596d44317aeeac4f09
drakon19 <danielbeis@hotmail.com>
2023-04-26 20:49:54 -0700

    ADD: variables

---
+++ variables.py
@@ -0,0 +1,6 @@
+name = "Daniel"
+age = 24
+list_shopping = "milk", "Eggs", "Vegetables"
+
+print(f"{name}, {age}") #f = literal templates, ahorras en el proceso de print(name + " + age")
+print(list_shopping)
