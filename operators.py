f20a27efaedd400d446721ead708261e1601a536
drakon19 <danielbeis@hotmail.com>
2023-04-26 20:49:06 -0700

    ADD: operators

---
+++ operators.py
@@ -0,0 +1,22 @@
+addition = 10 + 5 #Esto dara 15
+substraction = 10 - 5 #Esto dara 5
+multiplication = 10 * 5 #Esto dara 50
+
+### Comparation operators
+age = 10 == 5 #Esto dara false
+age = 10 != 5 #Esto dara True
+age = 10 > 5 #Esto dara True
+
+### Logical operators
+age_boy = 10
+age_girl = 14
+
+age_boy > 8 and age_girl > 8 #Esto dara true
+
+### Assigment operators
+age = 10 #Dara 10
+
+
+age += 5 #Esto dara 15
+age -= 5 #Esto dara 10
+age *= 5 #Esto dara 50
