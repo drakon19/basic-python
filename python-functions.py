3ff69b724eae98f1a8a76afd9ddf8269e04ae755
drakon19 <danielbeis@hotmail.com>
2023-05-03 19:36:37 -0700

    ADD: python functions

---
+++ python-functions.py
@@ -0,0 +1,15 @@
+#Tres ejemplos de funciones
+
+def suma(a, b):
+    suma = a+b
+    print(f"Tu suma es {suma}")
+
+def hola(nombre):
+    print(f"Hola mi estimado {nombre}")
+
+def trabajo(puesto, paga):
+    print(f"Tu puesto es {puesto}, y tu paga es de {paga}")
+
+suma(2, 5)
+hola("Daniel")
+trabajo("Rodolfo", 10000)
